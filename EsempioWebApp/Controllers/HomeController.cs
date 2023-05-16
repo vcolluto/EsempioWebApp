df3bf4b196fe0ae39462da74553303c7edd855a7
vcolluto <86895240+vcolluto@users.noreply.github.com>
2023-05-16 19:34:33 +0200

    Add project files.

---
+++ EsempioWebApp/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using EsempioWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EsempioWebApp.Controllers
+{
+    public class HomeController : Controller        //gestisce /Home/*
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()        //gestisce la richiesta /Home/Index  oppure /Home
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()      //gestisce la richiesta /Home/Privacy
+        {
+            return View();
+        }
+
+
+        public IActionResult Contatti()      //gestisce la richiesta /Home/Contatti
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ EsempioWebApp/Controllers/ProdottiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EsempioWebApp.Controllers
+{
+    public class ProdottiController : Controller    //gestisce /Prodotti/*
+    {
+        public IActionResult Index()        //gestisce /Prodotti/Index  oppure /Prodotti
+        {
+            return View();
+        }
+    }
+}
